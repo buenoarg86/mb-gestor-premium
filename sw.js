69713785d58c79cba89e444f0398152c26ae0234
buenoarg86 <buenoarg@gmail.com>
2026-09-09 23:56:12 -0300

    Atualizar cache do aplicativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mb-gestor-premium-v1';
+const CACHE='mb-gestor-premium-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png','./assets/logo-interna.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
